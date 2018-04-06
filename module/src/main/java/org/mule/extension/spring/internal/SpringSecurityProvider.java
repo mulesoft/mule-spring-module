e0f1da505a17a09a5faf97deb6156ba79c88ab29
afelisatti <afelisatti@mulesoft.com>
2018-04-06 15:08:37 -0300

    MULE-14711: Review spring module security configuration options

---
+++ module/src/main/java/org/mule/extension/spring/internal/SpringSecurityProvider.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.extension.spring.internal;
+
+/**
+ * Dummy class to allow proper POJO handling. Should be removed in the future.
+ */
+public class SpringSecurityProvider {
+
+}
