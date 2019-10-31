a31b81937c0041eee0dd8780e952f799948f9c3b
Luciano RaineriMarchina <lucianoRM@users.noreply.github.com>
2019-10-31 16:05:08 -0300

    MULE-17695: Spring Module extension model has the wrong suggested libraries version (#89)
    (cherry picked from commit 096ee6b69aa24a140334393ad5e2d8edcfd501eb)

--- tests/src/test/java/org/mule/extension/spring/test/SpringModuleAcceptVersionTestCase.java
+++ tests/src/test/java/org/mule/extension/spring/test/SpringModuleAcceptVersionTestCase.java
@@ -6,16 +6,21 @@
  */
 package org.mule.extension.spring.test;
 
+import static java.util.Optional.ofNullable;
+import static org.apache.commons.lang3.StringUtils.EMPTY;
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 import org.mule.extension.spring.internal.SpringModuleExtensionModelGenerator;
 import org.mule.runtime.api.exception.MuleRuntimeException;
+import org.mule.runtime.api.meta.model.declaration.fluent.ExtensionDeclaration;
 import org.mule.runtime.api.meta.model.declaration.fluent.ExtensionDeclarer;
 
 import java.io.File;
 import java.io.FileReader;
 import java.net.URISyntaxException;
+import java.util.Optional;
 import java.util.function.Supplier;
 
 import org.apache.maven.model.Model;
@@ -24,33 +29,80 @@ import org.junit.Test;
 
 public class SpringModuleAcceptVersionTestCase {
 
-  @Test
-  public void testVersion() {
+  private static final String SPRING_COORDINATES_PREFIX = "org.springframework:";
+  private static final String SPRING_SECURITY_COORDINATES_PREFIX = "org.springframework.security:";
+
+  private static final String SPRING_VERSION_PROPERTY = "springVersion";
+  private static final String SPRING_SECURITY_VERSION_PROPERTY = "springSecurityVersion";
+
+  private static final Supplier<File> POM_SUPPLIER = () -> {
+    try {
+      return new File(
+                      SpringModuleExtensionModelGenerator.class
+                          .getProtectionDomain()
+                          .getCodeSource()
+                          .getLocation()
+                          .toURI())
+                              .getParentFile()
+                              .getParentFile()
+                              .getParentFile();
+    } catch (URISyntaxException e) {
+      throw new MuleRuntimeException(e);
+    }
+  };
+
+  private final ExtensionDeclaration springExtensionModelDeclaration;
+
+  public SpringModuleAcceptVersionTestCase() {
     SpringModuleExtensionModelGenerator delegate = new SpringModuleExtensionModelGenerator();
     ExtensionDeclarer declarer = new ExtensionDeclarer();
     delegate.accept(declarer, null);
-    assertThat(declarer.getDeclaration().getVersion(), is(getModuleVersion()));
+    this.springExtensionModelDeclaration = declarer.getDeclaration();
   }
 
-  private static String getModuleVersion() {
-    return getMavenProperty("version", () -> {
-      try {
-        return new File(SpringModuleExtensionModelGenerator.class.getProtectionDomain().getCodeSource()
-            .getLocation().toURI()).getParentFile().getParentFile().getParentFile();
-      } catch (URISyntaxException e) {
-        throw new MuleRuntimeException(e);
-      }
-    });
+  @Test
+  public void testVersion() {
+    assertThat(
+               buildErrorMessage("spring-module"),
+               springExtensionModelDeclaration.getVersion(), is(getArtifactVersion()));
   }
 
-  private static String getMavenProperty(String property, Supplier<File> pomFolderFinder) {
-
-    Model mavenProject = mavenProject(pomFolderFinder);
-    return mavenProject.getVersion();
+  @Test
+  public void testSpringVersion() {
+    testVersionProperty(SPRING_VERSION_PROPERTY, SPRING_COORDINATES_PREFIX);
   }
 
-  private static Model mavenProject(Supplier<File> pomFolderFinder) {
-    return createMavenProject(new File(pomFolderFinder.get(), "pom.xml"));
+  @Test
+  public void testSpringSecurityVersion() {
+    testVersionProperty(SPRING_SECURITY_VERSION_PROPERTY, SPRING_SECURITY_COORDINATES_PREFIX);
+  }
+
+  private void testVersionProperty(String propertyName, String coordinatesPrefix) {
+    String expectedSpringProperty = getMavenProperty(propertyName)
+        .orElseThrow(() -> new RuntimeException("Unable to find property: " + propertyName + " declared in pom"));
+    springExtensionModelDeclaration.getExternalLibraryModels()
+        .stream()
+        .map(em -> em.getSuggestedCoordinates().orElse(EMPTY))
+        .filter(c -> c.startsWith(coordinatesPrefix))
+        .forEach(vc -> assertThat(buildErrorMessage(vc), vc, containsString(expectedSpringProperty)));
+  }
+
+  private String buildErrorMessage(String module) {
+    return module + " version declared in the extension model does not match the one defined in the pom.xml, check constants in "
+        + SpringModuleExtensionModelGenerator.class.getName();
+  }
+
+  private static String getArtifactVersion() {
+    return mavenProject().getVersion();
+  }
+
+  private static Optional<String> getMavenProperty(String propertyName) {
+    Model mavenProject = mavenProject();
+    return ofNullable(mavenProject.getProperties().getProperty(propertyName));
+  }
+
+  private static Model mavenProject() {
+    return createMavenProject(new File(POM_SUPPLIER.get(), "pom.xml"));
   }
 
   private static Model createMavenProject(File pomFile) {
@@ -59,17 +111,10 @@ public class SpringModuleAcceptVersionTestCase {
     }
     MavenXpp3Reader mavenReader = new MavenXpp3Reader();
 
-    try {
-      FileReader reader = new FileReader(pomFile);
-      try {
-        Model model = mavenReader.read(reader);
-        model.setPomFile(pomFile);
-        return model;
-      } finally {
-        if (reader != null) {
-          reader.close();
-        }
-      }
+    try (FileReader reader = new FileReader(pomFile)) {
+      Model model = mavenReader.read(reader);
+      model.setPomFile(pomFile);
+      return model;
     } catch (Exception e) {
       throw new RuntimeException("Couldn't get Maven Artifact from pom: " + pomFile, e);
     }
